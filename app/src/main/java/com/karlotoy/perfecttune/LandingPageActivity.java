567431ba13b46bcb8a89c3921e314142891bded9
unknown <pencil_010@163.com>
2019-05-01 20:10:26 +0800

    fix a bug and play all kinds of tunes

--- app/src/main/java/com/karlotoy/perfecttune/LandingPageActivity.java
+++ app/src/main/java/com/karlotoy/perfecttune/LandingPageActivity.java
@@ -7,14 +7,15 @@ import android.widget.Button;
 import android.widget.SeekBar;
 
 import com.karlotoy.perfectune.constants.TuneNote;
-import com.karlotoy.perfectune.thread.PerfectTune;
+import com.karlotoy.perfectune.thread.TuneThread;
 
+import java.lang.reflect.Field;
 
 public class LandingPageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SeekBar mSlider_1, mSlider_2;
     private double sliderval;
-    private PerfectTune perfectTune1, perfectTune2;
+    private TuneThread perfectTune1, perfectTune2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,10 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
         findViewById(R.id.stop_1_btn).setOnClickListener(this);
         findViewById(R.id.stop_2_btn).setOnClickListener(this);
 
-        perfectTune1 = new PerfectTune();
+        perfectTune1 = new TuneThread();
         perfectTune1.start();
 
-        perfectTune2 = new PerfectTune();
+        perfectTune2 = new TuneThread();
         perfectTune2.start();
 
         mSlider_1.setOnSeekBarChangeListener(getSeekBarChangeListener());
@@ -48,11 +49,50 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
                 if(b){
                     switch (seekBar.getId()){
                         case R.id.freq_1_sb:
-                            perfectTune1.setTuneFreq(sliderval * TuneNote.A4);
+                            //perfectTune1.setTuneFreq(sliderval * TuneNote.A4);
+
+                            Thread thread = new Thread(){
+                                public void run(){
+                                    TuneNote tn = new TuneNote();
+                                    allKindsOfTest(tn);
+                                }
+                            };
+                            thread.start();
                             break;
                         case R.id.freq_2_sb:
                             perfectTune2.setTuneFreq(sliderval * TuneNote.A4);
                             break;
+                    }
+                }
+            }
+
+            private void allKindsOfTest(Object obj){
+                Field[] field = obj.getClass().getDeclaredFields();
+
+                for(int i = 0; i < field.length; i++){
+                    String name = field[i].getName();
+                    try {
+                        boolean accessFlag = field[i].isAccessible();
+                        field[i].setAccessible(true);
+                        Object o;
+
+                        try {
+                            o = field[i].get(obj);
+                            //Log.e("LandingPage", "����Ķ����а���һ�����µı�����" + name + " = " + o);
+                            Double d = Double.parseDouble(o.toString());
+                            //Log.e("LandingPage", "����Ķ����а���һ�����µı�����" + name + " = " + d);
+
+                            perfectTune1.setTuneFreq(d);
+                            Thread.sleep(2000);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        field[i].setAccessible(accessFlag);
+                    } catch (IllegalArgumentException ex) {
+                        ex.printStackTrace();
                     }
                 }
             }
@@ -73,11 +113,11 @@ public class LandingPageActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.play_1_btn:
-                perfectTune1 = new PerfectTune();
+                perfectTune1 = new TuneThread();
                 perfectTune1.start();
                 break;
             case R.id.play_2_btn:
-                perfectTune2 = new PerfectTune();
+                perfectTune2 = new TuneThread();
                 perfectTune2.start();
                 break;
             case R.id.stop_1_btn:
